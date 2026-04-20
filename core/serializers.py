fcb84c7779619db0229f8f3b1967d193405917d0
Lucas Nunes <lucas.dev.nunes@gmail.com>
2026-04-20 14:01:59 -0300

    Adicao do campo prazo_formatado no serializer para exibicao no padrao brasileiro

--- core/serializers.py
+++ core/serializers.py
@@ -22,10 +22,11 @@ class TarefaSerializer(serializers.ModelSerializer):
     empresa_nome = serializers.CharField(source='empresa.nome')
     obrigacao_nome = serializers.CharField(source='obrigacao.nome')
     status_prazo = serializers.SerializerMethodField(method_name='calcular_prazo_status')
+    prazo_formatado = serializers.SerializerMethodField(method_name='formata_prazo')
 
     class Meta:
         model = Tarefa
-        fields = ['id', 'obrigacao_nome', 'status', 'status_prazo', 'prazo', 'empresa_nome']
+        fields = ['id', 'obrigacao_nome', 'status', 'status_prazo', 'prazo', 'prazo_formatado','empresa_nome']
 
     def calcular_prazo_status(self, obj):
         if not obj.prazo:
@@ -38,6 +39,11 @@ class TarefaSerializer(serializers.ModelSerializer):
             return "Prazo é hoje"
         else:
             return f"{delta} dias para o prazo"
+        
+    def formata_prazo(self, obj):
+        if not obj.prazo:
+            return None
+        return obj.prazo.strftime("%d/%m/%y")
 
 class TarefaDashboardSerializer(serializers.ModelSerializer):
     obrigacao_nome =serializers.CharField(source='obrigacao.nome')

6538196e7c40770a7370ec9c4c8431d0cb522d7c
Lucas Nunes <lucas.dev.nunes@gmail.com>
2026-04-23 13:18:35 -0300

    Melhoria do serializers de tarefas com cmapos read_only, padronizacao e formatacao de data

--- core/serializers.py
+++ core/serializers.py
@@ -19,31 +19,34 @@ class ObrigacaoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class TarefaSerializer(serializers.ModelSerializer):
-    empresa_nome = serializers.CharField(source='empresa.nome')
-    obrigacao_nome = serializers.CharField(source='obrigacao.nome')
-    status_prazo = serializers.SerializerMethodField(method_name='calcular_prazo_status')
-    prazo_formatado = serializers.SerializerMethodField(method_name='formata_prazo')
+    empresa_nome = serializers.CharField(source='empresa.nome', read_only=True)
+    obrigacao_nome = serializers.CharField(source='obrigacao.nome', read_only=True)
+    empresa_id = serializers.IntegerField(source='empresa.id', read_only=True)
+    obrigacao_id = serializers.IntegerField(source='obrigacao.id', read_only=True)
+    status_prazo = serializers.SerializerMethodField(method_name='calcular_prazo_status', read_only=True)
+    prazo_formatado = serializers.SerializerMethodField(method_name='formata_prazo', read_only=True)
 
     class Meta:
         model = Tarefa
-        fields = ['id', 'obrigacao_nome', 'status', 'status_prazo', 'prazo', 'prazo_formatado','empresa_nome']
+        fields = ['id', 'empresa_id', 'obrigacao_nome', 'obrigacao_id', 'obrigacao_nomedias','status', 'status_prazo', 'prazo', 'prazo_formatado','empresa_nome']
+        read_only_fields = ['status', 'concluida_em']
 
     def calcular_prazo_status(self, obj):
         if not obj.prazo:
             return "Sem prazo"
         hoje = timezone.localdate()
-        delta = (obj.prazo - hoje).days
-        if delta < 0:
-            return f"{abs(delta)} dias de atraso"
-        elif delta == 0:
+        dias = (obj.prazo - hoje).days
+        if dias < 0:
+            return f"{abs(dias)} dias de atraso"
+        elif dias == 0:
             return "Prazo é hoje"
         else:
-            return f"{delta} dias para o prazo"
+            return f"{dias} dias para o prazo"
         
     def formata_prazo(self, obj):
         if not obj.prazo:
             return None
-        return obj.prazo.strftime("%d/%m/%y")
+        return obj.prazo.strftime("%d/%m/%Y")
 
 class TarefaDashboardSerializer(serializers.ModelSerializer):
     obrigacao_nome =serializers.CharField(source='obrigacao.nome')
